84c72bfc40789c5c0400ffc27124129bd07ec75b
Johndeep <emkento.festa@volloeko.de>
2015-09-02 18:20:47 +0200

    * improvements of some manga server icons (updated to 32px) * fixed NineMangaCom, because it seems to use cloudflare too * some more server cleaning

--- app/src/main/java/ar/rulosoft/mimanganu/servers/ItNineMangaCom.java
+++ app/src/main/java/ar/rulosoft/mimanganu/servers/ItNineMangaCom.java
@@ -1,5 +1,7 @@
 package ar.rulosoft.mimanganu.servers;
 
+import android.text.Html;
+
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
@@ -8,47 +10,44 @@ import java.util.regex.Pattern;
 import ar.rulosoft.mimanganu.R;
 import ar.rulosoft.mimanganu.componentes.Chapter;
 import ar.rulosoft.mimanganu.componentes.Manga;
-import ar.rulosoft.navegadores.Navegador;
 
 public class ItNineMangaCom extends ServerBase {
-
-    public static String[] generos = new String[]{"Tutto", "0-9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
-            "U", "W", "X", "Y", "Z", "Action", "Adult", "Adventure", "Avventura", "Azione", "Bara", "Comedy", "Commedia", "Demenziale", "Doujinshi",
-            "Dounshinji", "Drama", "Ecchi", "Fantasy", "Gender Bender", "Harem", "Hentai", "Historical", "Horror", "Josei", "Magico", "Martial Arts", "Mature",
-            "Mecha", "Misteri", "Musica", "Mystery", "Psicologico", "Psychological", "Raccolta", "Romance", "Romantico", "School Life", "Sci-Fi", "Scolastico",
-            "Seinen", "Sentimentale", "Shota", "Shoujo", "Shounen", "Slice Of Life", "Smut", "Sovrannaturale", "Splatter", "Sportivo", "Sports", "Storico",
-            "Supernatural", "Tragedy", "Vita Quotidiana", "Yuri"};
-    public static String[] generosV = new String[]{"http://it.ninemanga.com/category/index_.html", "http://it.ninemanga.com/category/0-9_.html", "http://it.ninemanga.com/category/A_.html",
-            "http://it.ninemanga.com/category/B_.html", "http://it.ninemanga.com/category/C_.html", "http://it.ninemanga.com/category/D_.html",
-            "http://it.ninemanga.com/category/E_.html", "http://it.ninemanga.com/category/F_.html", "http://it.ninemanga.com/category/G_.html",
-            "http://it.ninemanga.com/category/H_.html", "http://it.ninemanga.com/category/I_.html", "http://it.ninemanga.com/category/J_.html",
-            "http://it.ninemanga.com/category/K_.html", "http://it.ninemanga.com/category/L_.html", "http://it.ninemanga.com/category/M_.html",
-            "http://it.ninemanga.com/category/N_.html", "http://it.ninemanga.com/category/O_.html", "http://it.ninemanga.com/category/P_.html",
-            "http://it.ninemanga.com/category/Q_.html", "http://it.ninemanga.com/category/R_.html", "http://it.ninemanga.com/category/S_.html",
-            "http://it.ninemanga.com/category/T_.html", "http://it.ninemanga.com/category/U_.html", "http://it.ninemanga.com/category/W_.html",
-            "http://it.ninemanga.com/category/X_.html", "http://it.ninemanga.com/category/Y_.html", "http://it.ninemanga.com/category/Z_.html",
-            "http://it.ninemanga.com/category/Action_.html", "http://it.ninemanga.com/category/Adult_.html", "http://it.ninemanga.com/category/Adventure_.html",
-            "http://it.ninemanga.com/category/Avventura_.html", "http://it.ninemanga.com/category/Azione_.html", "http://it.ninemanga.com/category/Bara_.html",
-            "http://it.ninemanga.com/category/Comedy_.html", "http://it.ninemanga.com/category/Commedia_.html",
-            "http://it.ninemanga.com/category/Demenziale_.html", "http://it.ninemanga.com/category/Doujinshi_.html",
-            "http://it.ninemanga.com/category/Dounshinji_.html", "http://it.ninemanga.com/category/Drama_.html", "http://it.ninemanga.com/category/Ecchi_.html",
-            "http://it.ninemanga.com/category/Fantasy_.html", "http://it.ninemanga.com/category/Gender+Bender_.html",
-            "http://it.ninemanga.com/category/Harem_.html", "http://it.ninemanga.com/category/Hentai_.html", "http://it.ninemanga.com/category/Historical_.html",
-            "http://it.ninemanga.com/category/Horror_.html", "http://it.ninemanga.com/category/Josei_.html", "http://it.ninemanga.com/category/Magico_.html",
-            "http://it.ninemanga.com/category/Martial+Arts_.html", "http://it.ninemanga.com/category/Mature_.html",
-            "http://it.ninemanga.com/category/Mecha_.html", "http://it.ninemanga.com/category/Misteri_.html", "http://it.ninemanga.com/category/Musica_.html",
-            "http://it.ninemanga.com/category/Mystery_.html", "http://it.ninemanga.com/category/Psicologico_.html",
-            "http://it.ninemanga.com/category/Psychological_.html", "http://it.ninemanga.com/category/Raccolta_.html",
-            "http://it.ninemanga.com/category/Romance_.html", "http://it.ninemanga.com/category/Romantico_.html",
-            "http://it.ninemanga.com/category/School+Life_.html", "http://it.ninemanga.com/category/Sci-Fi_.html",
-            "http://it.ninemanga.com/category/Scolastico_.html", "http://it.ninemanga.com/category/Seinen_.html",
-            "http://it.ninemanga.com/category/Sentimentale_.html", "http://it.ninemanga.com/category/Shota_.html",
-            "http://it.ninemanga.com/category/Shoujo_.html", "http://it.ninemanga.com/category/Shounen_.html",
-            "http://it.ninemanga.com/category/Slice+of+Life_.html", "http://it.ninemanga.com/category/Smut_.html",
-            "http://it.ninemanga.com/category/Sovrannaturale_.html", "http://it.ninemanga.com/category/Splatter_.html",
-            "http://it.ninemanga.com/category/Sportivo_.html", "http://it.ninemanga.com/category/Sports_.html", "http://it.ninemanga.com/category/Storico_.html",
-            "http://it.ninemanga.com/category/Supernatural_.html", "http://it.ninemanga.com/category/Tragedy_.html",
-            "http://it.ninemanga.com/category/Vita+Quotidiana_.html", "http://it.ninemanga.com/category/Yuri_.html"};
+    public static String HOST = "http://it.ninemanga.com";
+    public static String[] generos = new String[]{
+            "Tutto", "0-9",
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+            "N", "O", "P", "Q", "R", "S", "T", "U", "W", "X", "Y", "Z",
+            "Action", "Adult", "Adventure", "Avventura", "Azione", "Bara", "Comedy", "Commedia",
+            "Demenziale", "Doujinshi", "Dounshinji", "Drama", "Ecchi", "Fantasy", "Gender Bender",
+            "Harem", "Hentai", "Historical", "Horror", "Josei", "Magico", "Martial Arts", "Mature",
+            "Mecha", "Misteri", "Musica", "Mystery", "Psicologico", "Psychological", "Raccolta",
+            "Romance", "Romantico", "School Life", "Sci-Fi", "Scolastico", "Seinen",
+            "Sentimentale", "Shota", "Shoujo", "Shounen", "Slice Of Life", "Smut",
+            "Sovrannaturale", "Splatter", "Sportivo", "Sports", "Storico",
+            "Supernatural", "Tragedy", "Vita Quotidiana", "Yuri"
+    };
+    public static String[] generosV = new String[]{
+            "index_.html", "0-9_.html",
+            "A_.html", "B_.html", "C_.html", "D_.html", "E_.html", "F_.html", "G_.html",
+            "H_.html", "I_.html", "J_.html", "K_.html", "L_.html", "M_.html", "N_.html",
+            "O_.html", "P_.html", "Q_.html", "R_.html", "S_.html", "T_.html", "U_.html",
+            "W_.html", "X_.html", "Y_.html", "Z_.html",
+            "Action_.html", "Adult_.html", "Adventure_.html", "Avventura_.html", "Azione_.html",
+            "Bara_.html", "Comedy_.html", "Commedia_.html", "Demenziale_.html", "Doujinshi_.html",
+            "Dounshinji_.html", "Drama_.html", "Ecchi_.html", "Fantasy_.html",
+            "Gender+Bender_.html", "Harem_.html", "Hentai_.html", "Historical_.html",
+            "Horror_.html", "Josei_.html", "Magico_.html", "Martial+Arts_.html", "Mature_.html",
+            "Mecha_.html", "Misteri_.html", "Musica_.html", "Mystery_.html", "Psicologico_.html",
+            "Psychological_.html", "Raccolta_.html", "Romance_.html", "Romantico_.html",
+            "School+Life_.html", "Sci-Fi_.html", "Scolastico_.html", "Seinen_.html",
+            "Sentimentale_.html", "Shota_.html", "Shoujo_.html", "Shounen_.html",
+            "Slice+of+Life_.html", "Smut_.html", "Sovrannaturale_.html", "Splatter_.html",
+            "Sportivo_.html", "Sports_.html", "Storico_.html", "Supernatural_.html",
+            "Tragedy_.html", "Vita+Quotidiana_.html", "Yuri_.html"
+    };
+    static String[] order = new String[]{
+            "/category/", "/list/New-Update/", "/list/Hot-Book/", "/list/New-Book/"
+    };
 
     public ItNineMangaCom() {
         this.setFlag(R.drawable.flag_it);
@@ -59,18 +58,18 @@ public class ItNineMangaCom extends ServerBase {
 
     @Override
     public ArrayList<Manga> getMangas() throws Exception {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public ArrayList<Manga> search(String term) throws Exception {
-        String source = new Navegador().get("http://it.ninemanga.com/search/?wd=" + URLEncoder.encode(term, "UTF-8"));
+        String source = getNavWithHeader().get(
+                HOST + "/search/?wd=" + URLEncoder.encode(term, "UTF-8"));
         ArrayList<Manga> mangas = new ArrayList<>();
         Pattern p = Pattern.compile("bookname\" href=\"(/manga/[^\"]+)\">(.+?)<");
         Matcher m = p.matcher(source);
         while (m.find()) {
-            Manga manga = new Manga(ITNINEMANGA, m.group(2), "http://it.ninemanga.com" + m.group(1), false);
+            Manga manga = new Manga(ITNINEMANGA, m.group(2), HOST + m.group(1), false);
             mangas.add(manga);
         }
         return mangas;
@@ -84,30 +83,26 @@ public class ItNineMangaCom extends ServerBase {
 
     @Override
     public void loadMangaInformation(Manga m, boolean forceReload) throws Exception {
-        String source = new Navegador().get(m.getPath() + "?waring=1");
+        String source = getNavWithHeader().get(m.getPath() + "?waring=1");
         // portada
-        String portada = getFirstMatchDefault("Manga\" src=\"(.+?)\"", source, "");
-        m.setImages(portada);
+        m.setImages(getFirstMatchDefault("Manga\" src=\"(.+?)\"", source, ""));
         // sinopsis
-        String sinopsis = getFirstMatchDefault("<p itemprop=\"description\">(.+?)</p>", source, "Senza sinossi").replaceAll("<.+?>", "");
-        m.setSinopsis(sinopsis);
-
+        String sinopsis = getFirstMatchDefault("<p itemprop=\"description\">(.+?)</p>",
+                source, "Senza sinossi").replaceAll("<.+?>", "");
+        m.setSinopsis(Html.fromHtml(sinopsis.replaceFirst("Sommario:", "")).toString());
         // estado
         m.setFinished(getFirstMatchDefault("Stato:(.+?)</a>", source, "").contains("Completato"));
-
-        //autor
-
+        // autor
         m.setAuthor(getFirstMatchDefault("Author.+?\">(.+?)<", source, ""));
-
-        // cap�tulos
-        Pattern p = Pattern.compile("<a class=\"chapter_list_a\" href=\"(/chapter.+?)\" title=\"(.+?)\">(.+?)</a>");
+        // capitulos
+        Pattern p = Pattern.compile(
+                "<a class=\"chapter_list_a\" href=\"(/chapter.+?)\" title=\"(.+?)\">(.+?)</a>");
         Matcher matcher = p.matcher(source);
         ArrayList<Chapter> chapters = new ArrayList<>();
         while (matcher.find()) {
-            chapters.add(0, new Chapter(matcher.group(3), "http://it.ninemanga.com" + matcher.group(1)));
+            chapters.add(0, new Chapter(matcher.group(3), HOST + matcher.group(1)));
         }
         m.setChapters(chapters);
-
     }
 
     @Override
@@ -124,7 +119,8 @@ public class ItNineMangaCom extends ServerBase {
     }
 
     public void setExtra(Chapter c) throws Exception {
-        String source = new Navegador().get(c.getPath().replace(".html", "-" + c.getPages() + "-1.html"));
+        String source = getNavWithHeader().get(
+                c.getPath().replace(".html", "-" + c.getPages() + "-1.html"));
         Pattern p = Pattern.compile("<img class=\"manga_pic.+?src=\"([^\"]+)");
         Matcher m = p.matcher(source);
         String imagenes = "";
@@ -136,23 +132,28 @@ public class ItNineMangaCom extends ServerBase {
 
     @Override
     public void chapterInit(Chapter c) throws Exception {
-        String source = new Navegador().get(c.getPath());
-        String nop = getFirstMatch("\\d+/(\\d+)</option>[\\s]*</select>", source, "Error al obtener el n�mero de p�ginas");
+        String source = getNavWithHeader().get(c.getPath());
+        String nop = getFirstMatch(
+                "\\d+/(\\d+)</option>[\\s]*</select>", source,
+                "Error al obtener el n�mero de p�ginas");
         c.setPages(Integer.parseInt(nop));
     }
 
     @Override
     public ArrayList<Manga> getMangasFiltered(int categorie, int order, int pageNumber) throws Exception {
-        String source = new Navegador().get(generosV[categorie].replace("_", "_" + pageNumber));
+        String source = getNavWithHeader().get(
+                HOST + ItNineMangaCom.order[order] +
+                        generosV[categorie].replace("_", "_" + pageNumber));
         return getMangasFromSource(source);
     }
 
     public ArrayList<Manga> getMangasFromSource(String source) {
         ArrayList<Manga> mangas = new ArrayList<>();
-        Pattern p = Pattern.compile("<a href=\"(/manga/[^\"]+)\"><img src=\"(.+?)\".+?alt=\"([^\"]+)\"");
+        Pattern p = Pattern.compile(
+                "<a href=\"(/manga/[^\"]+)\"><img src=\"(.+?)\".+?alt=\"([^\"]+)\"");
         Matcher m = p.matcher(source);
         while (m.find()) {
-            Manga manga = new Manga(ITNINEMANGA, m.group(3), "http://it.ninemanga.com" + m.group(1), false);
+            Manga manga = new Manga(ITNINEMANGA, m.group(3), HOST + m.group(1), false);
             manga.setImages(m.group(2));
             mangas.add(manga);
         }
@@ -166,12 +167,12 @@ public class ItNineMangaCom extends ServerBase {
 
     @Override
     public String[] getOrders() {
-        return new String[]{"Popolarit�"};
+        // "/category/", "/list/New-Update/", "/list/Hot-Book", "/list/New-Book/"
+        return new String[]{"Lista Manga", "Ultime uscite", "Popolare Manga", "Nuovo Manga"};
     }
 
     @Override
     public boolean hasList() {
         return false;
     }
-
 }
