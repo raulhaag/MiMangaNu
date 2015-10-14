ad7d76ee5fadf2c656b3a798e11f02fc0cce06fa
Johndeep <emkento.festa@volloeko.de>
2015-10-14 13:41:25 +0200

    Removed unused Imports, re-upload name change.

--- app/src/main/java/ar/rulosoft/mimanganu/ActivityLicenseView.java
+++ app/src/main/java/ar/rulosoft/mimanganu/ActivityLicenseView.java
@@ -4,8 +4,6 @@ import android.content.SharedPreferences;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v4.app.NavUtils;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
