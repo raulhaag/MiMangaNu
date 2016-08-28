49b5424166bac4396ae7eec374f373fca0744173
xtj-9182 <databasev@yahoo.de>
2016-08-29 00:29:31 +0200

    this was only supposed to be disabled for debugging

--- app/src/main/java/ar/rulosoft/mimanganu/MiMangaNuApplication.java
+++ app/src/main/java/ar/rulosoft/mimanganu/MiMangaNuApplication.java
@@ -25,7 +25,7 @@ public class MiMangaNuApplication extends Application {
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
 
-        //ACRA.init(this);
+        ACRA.init(this);
     }
 
     public MiMangaNuApplication(){
