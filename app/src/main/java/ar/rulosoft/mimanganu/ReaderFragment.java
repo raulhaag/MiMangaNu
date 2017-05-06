9f206b08d02a090aff8d07aee63410feaefb6b91
Raul <raghaag@gmail.com>
2017-05-05 21:54:36 -0300

    if inutil

--- app/src/main/java/ar/rulosoft/mimanganu/ReaderFragment.java
+++ app/src/main/java/ar/rulosoft/mimanganu/ReaderFragment.java
@@ -345,17 +345,11 @@ public class ReaderFragment extends Fragment implements StateChangeListener, Dow
 
     private void hideSystemUI() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            if (pm.getBoolean("show_status_bar", false)) {
-                getActivity().getWindow().getDecorView().setSystemUiVisibility(
-                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                                | View.SYSTEM_UI_FLAG_IMMERSIVE);
-            } else {
-                getActivity().getWindow().getDecorView().setSystemUiVisibility(
-                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                                | View.SYSTEM_UI_FLAG_IMMERSIVE);
-            }
+            getActivity().getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE);
+
         }
     }
 
