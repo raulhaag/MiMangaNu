83a125e72980000fd42e5e77a7796bd0e5eac3ee
Tempura San <tempura.san@gmail.com>
2017-10-13 22:30:43 +0200

    JapScan: code cleanup
    tested according to ServerBase

--- app/src/main/java/ar/rulosoft/mimanganu/servers/JapScan.java
+++ app/src/main/java/ar/rulosoft/mimanganu/servers/JapScan.java
@@ -16,25 +16,21 @@ import ar.rulosoft.mimanganu.utils.Util;
 /**
  * Created by xtj-9182 on 21.02.2017.
  */
-
 class JapScan extends ServerBase {
 
-    public static String HOST = "http://www.japscan.com";
-
-    private static String[] order = new String[]{
-            "All"
-    };
-
-    private static String[] orderV = new String[]{
-            ""
-    };
+    private static final String HOST = "http://www.japscan.com";
 
     JapScan(Context context) {
         super(context);
-        this.setFlag(R.drawable.flag_fr);
-        this.setIcon(R.drawable.japscan);
-        this.setServerName("JapScan");
-        setServerID(ServerBase.JAPSCAN);
+        setFlag(R.drawable.flag_fr);
+        setIcon(R.drawable.japscan);
+        setServerName("JapScan");
+        setServerID(JAPSCAN);
+    }
+
+    @Override
+    public boolean hasList() {
+        return false;
     }
 
     @Override
@@ -59,40 +55,37 @@ class JapScan extends ServerBase {
 
     @Override
     public void loadChapters(Manga manga, boolean forceReload) throws Exception {
-        if (manga.getChapters() == null || manga.getChapters().size() == 0 || forceReload)
-            loadMangaInformation(manga, forceReload);
+        loadMangaInformation(manga, forceReload);
     }
 
     @Override
     public void loadMangaInformation(Manga manga, boolean forceReload) throws Exception {
-        String source = getNavigatorAndFlushParameters().get(manga.getPath());
+        if (manga.getChapters().isEmpty() || forceReload) {
+            String source = getNavigatorAndFlushParameters().get(manga.getPath());
 
-        // Cover Image
-        // JapScan has no cover images ...
+            // Cover Image
+            // JapScan has no cover images ...
 
-        // Summary
-        String summary = getFirstMatchDefault("<div id=\"synopsis\">(.+?)</div>", source, defaultSynopsis);
-        manga.setSynopsis(Util.getInstance().fromHtml(summary).toString());
+            // Summary
+            manga.setSynopsis(getFirstMatchDefault("<div id=\"synopsis\">(.+?)</div>", source, context.getString(R.string.nodisponible)));
 
-        // Status
-        manga.setFinished(!getFirstMatchDefault("<div class=\"row\">.+?<div class=\"cell\">.+?<div class=\"cell\">.+?<div class=\"cell\">.+?<div class=\"cell\">.+?<div class=\"cell\">(.+?)</div>", source, "").contains("En Cours"));
+            // Status
+            manga.setFinished(!getFirstMatchDefault("<div class=\"row\">.+?<div class=\"cell\">.+?<div class=\"cell\">.+?<div class=\"cell\">.+?<div class=\"cell\">.+?<div class=\"cell\">(.+?)</div>", source, "").contains("En Cours"));
 
-        // Author
-        manga.setAuthor(Util.getInstance().fromHtml(getFirstMatchDefault("<div class=\"row\">(.+?)</div>", source, "")).toString().trim());
+            // Author
+            manga.setAuthor(getFirstMatchDefault("<div class=\"row\">(.+?)</div>", source, context.getString(R.string.nodisponible)));
 
-        // Genres
-        manga.setGenre((Util.getInstance().fromHtml(getFirstMatchDefault("<div class=\"row\">.+?<div class=\"cell\">.+?<div class=\"cell\">.+?<div class=\"cell\">(.+?)</div>", source, "")).toString().trim()));
+            // Genres
+            manga.setGenre(getFirstMatchDefault("<div class=\"row\">.+?<div class=\"cell\">.+?<div class=\"cell\">.+?<div class=\"cell\">(.+?)</div>", source, context.getString(R.string.nodisponible)));
 
-        // Chapters
-        Pattern pattern = Pattern.compile("<a href=\"(//www\\.japscan\\.com/lecture-en-ligne/[^\"]+?)\">(Scan.+?)</a>", Pattern.DOTALL);
-        Matcher matcher = pattern.matcher(source);
-        ArrayList<Chapter> chapters = new ArrayList<>();
-        while (matcher.find()) {
-            /*Log.d("JS", "1: " + "http:" + matcher.group(1));
-            Log.d("JS", "2: " + matcher.group(2));*/
-            chapters.add(0, new Chapter(Util.getInstance().fromHtml(matcher.group(2)).toString(), "http:" + matcher.group(1)));
+            // Chapters
+            Pattern pattern = Pattern.compile("<a href=\"(//www\\.japscan\\.com/lecture-en-ligne/[^\"]+?)\">(Scan.+?)</a>", Pattern.DOTALL);
+            Matcher matcher = pattern.matcher(source);
+            while (matcher.find()) {
+                Chapter chapter = new Chapter(matcher.group(2), "http:" + matcher.group(1));
+                chapter.addChapterFirst(manga);
+            }
         }
-        manga.setChapters(chapters);
     }
 
     @Override
@@ -102,25 +95,15 @@ class JapScan extends ServerBase {
 
     @Override
     public String getImageFrom(Chapter chapter, int page) throws Exception {
-        //Log.d("JS", "source: " + chapter.getPath() + page + ".html");
         String source = getNavigatorAndFlushParameters().get(chapter.getPath() + page + ".html");
-        String img = "";
-        try {
-            img = getFirstMatchDefault("src=\"(http://cdn[^\"]+)", source, "Error getting image");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        //Log.d("JS", "img: " + img);
-        return img;
+        return getFirstMatchDefault("src=\"(http://cdn[^\"]+)", source, "Error: failed to locate page image link");
     }
 
     @Override
     public void chapterInit(Chapter chapter) throws Exception {
         String source = getNavigatorAndFlushParameters().get(chapter.getPath());
-        //Log.d("JS", "p: " + chapter.getPath());
-        String pagenumber = getFirstMatchDefault("Page (\\d+)</option>[\\s]*</select>", source, "failed to get the number of pages");
-        //Log.d("JS", "pa: " + pagenumber);
-        chapter.setPages(Integer.parseInt(pagenumber));
+        String pages = getFirstMatchDefault("Page (\\d+)</option>[\\s]*</select>", source, "Error: failed to get the number of pages");
+        chapter.setPages(Integer.parseInt(pages));
     }
 
     private ArrayList<Manga> getMangasFromSource(String source) {
@@ -128,8 +111,6 @@ class JapScan extends ServerBase {
         Matcher matcher = pattern.matcher(source);
         ArrayList<Manga> mangas = new ArrayList<>();
         while (matcher.find()) {
-            /*Log.d("JS", "1: " + matcher.group(1));
-            Log.d("JS", "2: " + matcher.group(2));*/
             Manga manga = new Manga(getServerID(), matcher.group(2), HOST +  matcher.group(1), false);
             mangas.add(manga);
         }
@@ -143,24 +124,7 @@ class JapScan extends ServerBase {
 
     @Override
     public ArrayList<Manga> getMangasFiltered(int[][] filters, int pageNumber) throws Exception {
-        String web = HOST;
-        if (orderV[filters[0][0]].equals("")) {
-            web = "http://www.japscan.com/mangas/";
-        }
-        //Log.d("JS","web: "+web);
-        String source = getNavigatorAndFlushParameters().get(web);
+        String source = getNavigatorAndFlushParameters().get(HOST + "/mangas/");
         return getMangasFromSource(source);
-    }
-
-    @Override
-    public ServerFilter[] getServerFilters() {
-        return new ServerFilter[]{
-                new ServerFilter("Order", order, ServerFilter.FilterType.SINGLE)
-        };
-    }
-
-    @Override
-    public boolean hasList() {
-        return false;
     }
 }
