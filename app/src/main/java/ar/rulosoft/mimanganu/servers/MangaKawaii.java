e8d20472aa2e57b5d03803c1dae626292f0ef12a
Tempura San <tempura.san@gmail.com>
2017-10-17 21:28:27 +0200

    MangaKawaii: code cleanup
    tested according to ServerBase
    removed broken search functionality
    added TODOs and FIXMEs

--- app/src/main/java/ar/rulosoft/mimanganu/servers/MangaKawaii.java
+++ app/src/main/java/ar/rulosoft/mimanganu/servers/MangaKawaii.java
@@ -1,7 +1,7 @@
 package ar.rulosoft.mimanganu.servers;
 
 import android.content.Context;
-import android.util.Log;
+import android.text.TextUtils;
 
 import java.util.ArrayList;
 import java.util.regex.Matcher;
@@ -10,101 +10,79 @@ import java.util.regex.Pattern;
 import ar.rulosoft.mimanganu.R;
 import ar.rulosoft.mimanganu.componentes.Chapter;
 import ar.rulosoft.mimanganu.componentes.Manga;
-import ar.rulosoft.mimanganu.componentes.ServerFilter;
-import ar.rulosoft.mimanganu.utils.Util;
 
 /**
  * Created by xtj-9182 on 09.05.2017.
  */
 /* Je suis Charlie */
 class MangaKawaii extends ServerBase {
-    private static String HOST = "https://www.mangakawaii.com";
-    private static String[] genre = new String[]{
-            "All"
-    };
+    private static final String HOST = "https://www.mangakawaii.com";
 
     MangaKawaii(Context context) {
         super(context);
-        this.setFlag(R.drawable.flag_fr);
-        this.setIcon(R.drawable.mangakawaii);
-        this.setServerName("MangaKawaii");
-        setServerID(ServerBase.MANGAKAWAII);
+        setFlag(R.drawable.flag_fr);
+        setIcon(R.drawable.mangakawaii);
+        setServerName("MangaKawaii");
+        setServerID(MANGAKAWAII);
+    }
+
+    @Override
+    public boolean hasList() {
+        return false;
     }
 
     @Override
     public ArrayList<Manga> getMangas() throws Exception {
+        // FIXME a list of Manga could be retrieved using JavaScript
         return null;
     }
 
     @Override
     public ArrayList<Manga> search(String search) throws Exception {
-        ArrayList<Manga> mangas = new ArrayList<>();
-        String web = "https://www.mangakawaii.com/liste-mangas-texte";
-        String source = getNavigatorAndFlushParameters().get(web);
-        Pattern pattern = Pattern.compile("href=\"(https://www\\.mangakawaii\\.com/manga/[^\"]+?)\" style=\"display: inline-block\"><h6 style=\"margin: 0\">([^\"]+?)</h6></a>", Pattern.DOTALL); //manga-list(.+?)</ul>
-        Matcher matcher = pattern.matcher(source);
-        while (matcher.find()) {
-            /*Log.d("MKA", "1: " + matcher.group(1));
-            Log.d("MKA", "2: " + matcher.group(2));*/
-            if (matcher.group(2).toLowerCase().contains(search.toLowerCase())) {
-                    /*Log.d("MKA", "1: " + matcher.group(1));
-                    Log.d("MKA", "2: " + matcher.group(2));*/
-                Manga manga = new Manga(getServerID(), matcher.group(2), matcher.group(1), false);
-                mangas.add(manga);
-            }
-        }
-        return mangas;
+        // FIXME search functionality has to be implemented bases on Manga list
+        return null;
     }
 
     @Override
     public void loadChapters(Manga manga, boolean forceReload) throws Exception {
-        if (manga.getChapters() == null || manga.getChapters().size() == 0 || forceReload)
-            loadMangaInformation(manga, forceReload);
+        loadMangaInformation(manga, forceReload);
     }
 
     @Override
     public void loadMangaInformation(Manga manga, boolean forceReload) throws Exception {
-        String source = getNavigatorAndFlushParameters().get(manga.getPath());
+        if (manga.getChapters().isEmpty() || forceReload) {
+            String source = getNavigatorAndFlushParameters().get(manga.getPath());
 
-        // Cover
-        if (manga.getImages() == null || manga.getImages().isEmpty()) {
-            String img = getFirstMatchDefault("src='(https://www.mangakawaii.com/uploads/manga/[^\"]+?)'", source, "");
-            manga.setImages(img);
-        }
+            // Cover
+            if (manga.getImages() == null || manga.getImages().isEmpty()) {
+                String img = getFirstMatchDefault("src='(https://www.mangakawaii.com/uploads/manga/[^\"]+?)'", source, "");
+                manga.setImages(img);
+            }
 
-        // Summary
-        String summary = getFirstMatchDefault("id=\"synopsis\">[\\s]*<p>(.+?)</p>", source, "");
-        manga.setSynopsis(Util.getInstance().fromHtml(summary.trim()).toString());
+            // Summary
+            manga.setSynopsis(getFirstMatchDefault("id=\"synopsis\">[\\s]*<p>(.+?)</p>", source, context.getString(R.string.nodisponible)));
 
-        // Status
-        boolean status = !getFirstMatchDefault("Statut(.+?)</span>", source, "").contains("En Cours");
-        manga.setFinished(status);
+            // Status
+            manga.setFinished(!getFirstMatchDefault("Statut(.+?)</span>", source, "").contains("En Cours"));
 
-        // Author
-        String author = getFirstMatchDefault("author/.+?\">(.+?)</a>", source, "");
-        manga.setAuthor(author);
+            // Author
+            manga.setAuthor(getFirstMatchDefault("author/.+?\">(.+?)</a>", source, context.getString(R.string.nodisponible)));
 
-        // Genre
-        String genre = Util.getInstance().fromHtml(getFirstMatchDefault("Genres</span>(.+?)</span>", source, "")).toString();
-        //Log.d("MKA", "g: " + genre);
-        manga.setGenre(genre.replaceAll(",,",","));
+            // Genre
+            manga.setGenre(getFirstMatchDefault("Genres</span>(.+?)</span>", source, context.getString(R.string.nodisponible)));
 
-        // Chapters
-        Pattern p = Pattern.compile("href=\"(https://www\\.mangakawaii\\.com/manga/[^\"]+?)\">([^\"]+?)</a>", Pattern.DOTALL);
-        Matcher matcher = p.matcher(source);
-        ArrayList<Chapter> chapters = new ArrayList<>();
-        ArrayList<String> tmpChapterList = new ArrayList<>();
-        while (matcher.find()) {
-            /*Log.d("MKA", "(1): " + matcher.group(1));
-            Log.d("MKA", "(2): " + matcher.group(2));*/
-            if(!tmpChapterList.contains(matcher.group(1))) {
-                /*Log.d("MKA", "(1): " + matcher.group(1));
-                Log.d("MKA", "(2): " + matcher.group(2));*/
-                chapters.add(0, new Chapter(matcher.group(2), matcher.group(1)));
-                tmpChapterList.add(matcher.group(1));
+            // Chapters
+            Pattern p = Pattern.compile("href=\"(https://www\\.mangakawaii\\.com/manga/[^\"]+?)\">([^\"]+?)</a>", Pattern.DOTALL);
+            Matcher matcher = p.matcher(source);
+            ArrayList<String> tmpChapterList = new ArrayList<>();
+            while (matcher.find()) {
+                if(!tmpChapterList.contains(matcher.group(1))) {
+                    Chapter chapter = new Chapter(matcher.group(2), matcher.group(1));
+                    chapter.addChapterFirst(manga);
+                    tmpChapterList.add(matcher.group(1));
+                }
             }
         }
-        manga.setChapters(chapters);
     }
 
     @Override
@@ -121,70 +99,43 @@ class MangaKawaii extends ServerBase {
     }
 
     private int setExtra(Chapter chapter) throws Exception {
-        int pages = 0;
-        String source = "";
-        try {
-            source = getNavigatorAndFlushParameters().get(chapter.getPath());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        String tmpImages = getFirstMatchDefault("<div id=\"all\"(.+?)</div>", source,"");
-        Pattern pattern = Pattern.compile("data-src='.(https://www\\.mangakawaii\\.com/uploads/[^\"]+?).'", Pattern.DOTALL);
-        Matcher matcher = pattern.matcher(tmpImages);
-        String images = "";
-        while (matcher.find()) {
-            pages++;
-            //Log.d("MKA", "1: " + matcher.group(1));
-            images = images + "|" + matcher.group(1);
-        }
-        chapter.setExtra(images);
-        return pages;
+        String source = getNavigatorAndFlushParameters().get(chapter.getPath());
+        String tmpImages = getFirstMatchDefault("<div id=\"all\"(.+?)</div>", source, "");
+
+        ArrayList<String> matches = getAllMatch("data-src='.(https://www\\.mangakawaii\\.com/uploads/[^\"]+?).'", tmpImages);
+        chapter.setExtra(TextUtils.join("|", matches));
+
+        return matches.size();
     }
 
     @Override
     public void chapterInit(Chapter chapter) throws Exception {
         if (chapter.getExtra() == null || chapter.getExtra().length() < 2) {
             chapter.setPages(setExtra(chapter));
-        } else
+        } else {
             chapter.setPages(0);
-    }
-
-    @Override
-    public ServerFilter[] getServerFilters() {
-        return new ServerFilter[]{
-                new ServerFilter("Genre(s)", genre, ServerFilter.FilterType.SINGLE),
-        };
-    }
-
-    private ArrayList<Manga> getMangasFromSource(String source) {
-        Pattern pattern = Pattern.compile("(<img w.+? (class=\"thumbnail\">|inline-block;\", Pattern.DOTALL))");
-        Matcher matcher = pattern.matcher(source);
-        ArrayList<Manga> mangas = new ArrayList<>();
-        while (matcher.find()) {
-            /*Log.d("MKA", "(0): " + matcher.group(1));
-            Log.d("MKA", "(1): " + getFirstMatchDefault("href=\"([^\"]+?)\"", matcher.group(1), ""));
-            Log.d("MKA", "(2): " + getFirstMatchDefault("src='([^\"]+?)'", matcher.group(1), ""));
-            Log.d("MKA", "(3): " + getFirstMatchDefault("alt='([^\"]+?)'", matcher.group(1), ""));*/
-            Manga m = new Manga(getServerID(), getFirstMatchDefault("alt='([^\"]+?)'", matcher.group(1), ""), getFirstMatchDefault("href=\"([^\"]+?)\"", matcher.group(1), ""), false);
-            m.setImages(getFirstMatchDefault("src='([^\"]+?)'", matcher.group(1), ""));
-            mangas.add(m);
         }
-        return mangas;
     }
 
     @Override
     public ArrayList<Manga> getMangasFiltered(int[][] filters, int pageNumber) throws Exception {
-        String web = "https://www.mangakawaii.com/liste-mangas?page=" + pageNumber;
-        Log.d("MKA", "web: " + web);
-        String source = getNavigatorAndFlushParameters().get(web);
-        if (source.isEmpty()) // page 3 is broken for some reason ¯\_(ツ)_/¯
-            source = getNavigatorAndFlushParameters().get(web + (pageNumber + 1));
-        return getMangasFromSource(source);
-    }
+        // TODO implement filtering
 
-    @Override
-    public boolean hasList() {
-        return false;
+        String web = HOST + "/liste-mangas?page=" + pageNumber;
+        String source = getNavigatorAndFlushParameters().get(web);
+
+        Pattern pattern = Pattern.compile("<div class=\"media-left\">(.+?)</div>", Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(source);
+        ArrayList<Manga> mangas = new ArrayList<>();
+        while (matcher.find()) {
+            Manga m = new Manga(getServerID(),
+                    getFirstMatchDefault("alt='([^']+)", matcher.group(1), ""),
+                    getFirstMatchDefault("href=\"([^\"]+)", matcher.group(1), ""),
+                    false
+            );
+            m.setImages(getFirstMatchDefault("src='([^\']+)", matcher.group(1), ""));
+            mangas.add(m);
+        }
+        return mangas;
     }
 }
-
