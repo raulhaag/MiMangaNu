46e200b0cd4eb934681a2a66d2224a48fbb4d7e4
raulhaag <raghaag@gmail.com>
2017-12-08 12:30:24 -0300

    fix mangafox search

--- app/src/main/java/ar/rulosoft/mimanganu/servers/MangaFox.java
+++ app/src/main/java/ar/rulosoft/mimanganu/servers/MangaFox.java
@@ -240,7 +240,7 @@ class MangaFox extends ServerBase {
     public ArrayList<Manga> search(String term) throws Exception {
         ArrayList<Manga> mangas = new ArrayList<>();
         String data = getNavigatorAndFlushParameters()
-                .get("https://mangafox.me/search.php?name_method=cw&name="
+                .get("http://mangafox.me/search.php?name_method=cw&name="
                         + URLEncoder.encode(term.replaceAll(" ", "+"), "UTF-8")
                         + "&type=&author_method=cw&author=&artist_method=cw&artist=&genres%5BAction%5D=0&genres%5BAdult%5D=0&genres%5BAdventure%5D=0&genres%5BComedy%5D=0&genres%5BDoujinshi%5D=0&genres%5BDrama%5D=0&genres%5BEcchi%5D=0&genres%5BFantasy%5D=0&genres%5BGender+Bender%5D=0&genres%5BHarem%5D=0&genres%5BHistorical%5D=0&genres%5BHorror%5D=0&genres%5BJosei%5D=0&genres%5BMartial+Arts%5D=0&genres%5BMature%5D=0&genres%5BMecha%5D=0&genres%5BMystery%5D=0&genres%5BOne+Shot%5D=0&genres%5BPsychological%5D=0&genres%5BRomance%5D=0&genres%5BSchool+Life%5D=0&genres%5BSci-fi%5D=0&genres%5BSeinen%5D=0&genres%5BShoujo%5D=0&genres%5BShoujo+Ai%5D=0&genres%5BShounen%5D=0&genres%5BShounen+Ai%5D=0&genres%5BSlice+of+Life%5D=0&genres%5BSmut%5D=0&genres%5BSports%5D=0&genres%5BSupernatural%5D=0&genres%5BTragedy%5D=0&genres%5BWebtoons%5D=0&genres%5BYaoi%5D=0&genres%5BYuri%5D=0&released_method=eq&released=&rating_method=eq&rating=&is_completed=&advopts=1");
         Pattern p = Pattern.compile(PATTERN_MANGA_SEARCH, Pattern.DOTALL);
