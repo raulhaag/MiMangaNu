1d3b6261fb39a2c0e0ac4246fcd7186055655bbd
raulhaag <raghaag@gmail.com>
2018-08-30 20:39:25 -0300

    changes

---
+++ fakegps/src/main/java/com/google/android/gms/security/ProviderInstaller.java
@@ -0,0 +1,9 @@
+package com.google.android.gms.security;
+
+import android.content.Context;
+
+public class ProviderInstaller {
+    public static void installIfNeeded(Context context){
+        //dummy
+    }
+}
