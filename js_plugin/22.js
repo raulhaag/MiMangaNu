4192f4d78ba37b4c71aad88ed7edf68c7419315a
raulhaag <raghaag@gmail.com>
2018-09-08 04:29:18 -0300

    Hybrid plugin test

--- js_plugin/22.js
+++ js_plugin/22.js
@@ -1,22 +1,22 @@
-function printer(data) {
-	siteR = /(https:\\/\\/[img1]*.tumangaonline.me\\/uploads\\/[^\\\\\\\"^']+)/g;
+function chapterInit(data) {
+	siteR = new RegExp("(https://[img1]*.tumangaonline.me/uploads/[^\"^']+)");
 	path = siteR.exec(data)[0];
-	idsR = /<canvas id=\\\"([^\\\"]+)/g;
-	dataa = "";
+	idsR = new RegExp("<canvas id=\"([^\"]+)");
+	idData = "";
 	do{
 		m = idsR.exec(data);
 		if(m){
-			dataa = dataa + m[1] + \"|\";
+			idData = idData + m[1] + "|";
 		}
 	}while(m);
-	ids = dataa.split(\"|\");
+	ids = idData.split(\"|\");
 	idsL = ids.length - 1;
 	images = \"\";
 	for(i = 0; i < idsL; i++) {
-		rs = ids[i] + \".{0,100}?'([^']+.jpg)\";
+		rs = ids[i] + ".{0,100}?'([^']+.jpg)";
 		fid = new RegExp(rs);
 		mid = fid.exec(data);
-		images = images + \"|\" + path + mid[1];
+		images = images + "|" + path + mid[1];
 	}
 	return images;
 }
