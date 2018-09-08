00565b0fff6942e7f2345927c744cd28617fff62
raulhaag <raghaag@gmail.com>
2018-09-08 04:45:10 -0300

    Hybrid plugin test

--- js_plugin/22.js
+++ js_plugin/22.js
@@ -1,22 +1,23 @@
 function chapterInit(data) {
-	siteR = new RegExp("(https://[img1]*.tumangaonline.me/uploads/[^\"^']+)");
-	path = siteR.exec(data)[0];
-	idsR = new RegExp("<canvas id=\"([^\"]+)");
-	idData = "";
-	do{
-		m = idsR.exec(data);
-		if(m){
-			idData = idData + m[1] + "|";
-		}
-	}while(m);
-	ids = idData.split(\"|\");
-	idsL = ids.length - 1;
-	images = \"\";
-	for(i = 0; i < idsL; i++) {
-		rs = ids[i] + ".{0,100}?'([^']+.jpg)";
-		fid = new RegExp(rs);
-		mid = fid.exec(data);
-		images = images + "|" + path + mid[1];
-	}
-	return images;
+    pipe "|";
+    siteR = /(https:\/\/[img1]*.tumangaonline.me\/uploads\/[^\\\"^']+)/g;
+    path = siteR.exec(data)[0];
+    idsR = /<canvas id=\"([^\"]+)/g;
+    idData = ""
+    do {
+        m = idsR.exec(data);
+        if (m) {
+            idData = idData + m[1] + pipe;
+        }
+    } while (m);
+    ids = idData.split(pipe);
+    idsL = ids.length - 1;
+    images = "";
+    for (i = 0; i < idsL; i++) {
+        rs = ids[i] + ".{0,100}?'([^']+.jpg)";
+        fid = new RegExp(rs);
+        mid = fid.exec(data);
+        images = images + pipe + path + mid[1];
+    }
+    return images;
 }
