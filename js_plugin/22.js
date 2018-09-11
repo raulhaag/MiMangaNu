d94963ac0e6523b6931d0be3d0a7b633ddc223cb
raulhaag <raghaag@gmail.com>
2018-09-11 12:39:50 -0300

    fix tmo

--- js_plugin/22.js
+++ js_plugin/22.js
@@ -1,23 +1,12 @@
 function chapterInit(data) {
-    pipe = "|";
-    siteR = /(https:\/\/[img1]*.tumangaonline.me\/uploads\/[^\\\"^']+)/g;
-    path = siteR.exec(data)[0];
-    idsR = /<canvas id=\"([^\"]+)/g;
-    idData = ""
-    do {
-        m = idsR.exec(data);
-        if (m) {
-            idData = idData + m[1] + pipe;
-        }
-    } while (m);
-    ids = idData.split(pipe);
-    idsL = ids.length - 1;
-    images = "";
-    for (i = 0; i < idsL; i++) {
-        rs = ids[i] + ".{0,100}?'([^']+.jpg)";
-        fid = new RegExp(rs);
-        mid = fid.exec(data);
-        images = images + pipe + path + mid[1];
-    }
-    return images;
-}
+     pipe = "|";
+     idsR = /;" src="(https:[^"]+)/g;
+     idData = ""
+     do {
+         m = idsR.exec(data);
+         if (m) {
+             idData = idData + pipe + m[1] ;
+         }
+     } while (m);
+     return idData;
+ }
