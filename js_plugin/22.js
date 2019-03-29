2ce0d2ce3b2d1d1baf6d789dff6d05309a36e524
raulhaag <2048339+raulhaag@users.noreply.github.com>
2019-03-29 09:18:59 -0300

    tmoupdateplugin

--- js_plugin/22.js
+++ js_plugin/22.js
@@ -1,12 +1,12 @@
 function chapterInit(data) {
-     pipe = "|";
-     idsR = /;" src="(https:[^"]+)/g;
-     idData = ""
-     do {
-         m = idsR.exec(data);
-         if (m) {
-             idData = idData + pipe + m[1] ;
-         }
-     } while (m);
-     return idData;
- }
+       pipe = "|";
+       idsR = /<img[^>]+" src="([^"]+)/gm;
+       idData = ""
+       do {
+           m = idsR.exec(data);
+           if (m) {
+               idData = idData + pipe + m[1] ;
+           }
+       } while (m);
+       return idData;
+   }
