07821b2f0780d375a256555495595d6c7c9da62c
raulhaag <2048339+raulhaag@users.noreply.github.com>
2019-11-05 15:23:46 -0300

    new remote version of tmo

--- js_plugin/22.js
+++ js_plugin/22.js
@@ -1,21 +1,31 @@
 function chapterInit(data) {
-  pipe = "|";
-  idsR = /<img[^>]+src="([^"]+)" class="viewer-image"/gm;
-  idData = ""
-  do {
-    m = idsR.exec(data);
-    if (m) {
-      idData = idData + pipe + m[1] ;
-    }
-  } while (m);
-  if(idData == ""){
-    idsR = /img_[^\.^;]+\.src\s*=\s*"([^"]+)/gm;
-    do {
-      m = idsR.exec(data);
-      if (m) {
-        idData = idData + pipe + m[1] ;
-      }
-    } while (m);
-  }
-  return idData;
- }
+	pipe = '|';
+	var web = nav.getRedirectWeb(data, 'Referer|' + data);
+	var id = web.split('/')[4];
+	var imgBase = 'https://img1.tmofans.com/uploads/' + id + '/';
+	var token = nav.get('https://tmofans.com/viewer/' + id + "/cascade", 'Referer|' + data);
+	rTok = /<meta name="csrf-token" content="([^"]+)\">/gm;
+	rE = /e\('([^']+)','([^']+)','([^']+)'\)/gm;
+	t = rTok.exec(token);
+	if(!t){
+	  return '';
+	}
+	e = rE.exec(token);
+	if(!e){
+	  return '';
+	}
+	headers = "Referer|https://tmofans.com/viewer/" + id + "/cascade";
+	headers = headers + "|X-CSRF-TOKEN|" + t[1];
+	headers = headers + "|X-Requested-With|XMLHttpRequest";
+	imgweb = "https://tmofans.com/upload_images/" + id + "/" + e[2] +"/all";
+	imgs = nav.post(imgweb, headers, '');
+	rImg = /"([^"^,^\]]+)/gm;
+	oImg = "https://tmofans.com/viewer/" + id + "/cascade";
+	do {
+		i = rImg.exec(imgs);
+		if (i) {
+			oImg = oImg + pipe + imgBase + i[1];
+		}
+	} while (i);
+	return oImg;
+}
