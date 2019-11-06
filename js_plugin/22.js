5888d0a3bdde94ad9bfe68489240c73356a656b8
Raul <2048339+raulhaag@users.noreply.github.com>
2019-11-06 10:27:18 -0300

    Update 22.js
    this is boring

--- js_plugin/22.js
+++ js_plugin/22.js
@@ -2,28 +2,13 @@ function chapterInit(data) {
 	pipe = '|';
 	var web = nav.getRedirectWeb(data, 'Referer|' + data);
 	var id = web.split('/')[4];
-	var imgBase = 'https://img1.tmofans.com/uploads/' + id + '/';
-	var token = nav.get('https://tmofans.com/viewer/' + id + "/cascade", 'Referer|' + data);
-	rTok = /<meta name="csrf-token" content="([^"]+)\">/gm;
-	rE = /e\('([^']+)','([^']+)','([^']+)'\)/gm;
-	t = rTok.exec(token);
-	if(!t){
-	  return '';
-	}
-	e = rE.exec(token);
-	if(!e){
-	  return '';
-	}
-	headers = "Referer|https://tmofans.com/viewer/" + id + "/cascade";
-	headers = headers + "|X-CSRF-TOKEN|" + t[1];
-	imgweb = "https://tmofans.com/upload_images/" + id + "/" + e[2] +"/all";
-	imgs = nav.post(imgweb, headers, '');
-	rImg = /"([^"^,^\]]+)/gm;
+	var src = nav.get('https://tmofans.com/viewer/' + id + "/cascade", 'Referer|' + data);
+	rImg = /(https:..img1.tmofans.com.uploads.[^"]+)/gm;
 	oImg = "https://tmofans.com/viewer/" + id + "/cascade";
 	do {
-		i = rImg.exec(imgs);
+		i = rImg.exec(src);
 		if (i) {
-			oImg = oImg + pipe + imgBase + i[1];
+			oImg = oImg + pipe + i[1];
 		}
 	} while (i);
 	return oImg;
