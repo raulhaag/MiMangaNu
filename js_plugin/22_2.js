d5423a1d5acdf0fb73dfa0b2cbe8f0e7c2b7be48
Raul <2048339+raulhaag@users.noreply.github.com>
2019-11-13 15:10:12 -0300

    Create 22_2.js

---
+++ js_plugin/22_2.js
@@ -0,0 +1,27 @@
+function chapterInit(cw, mw) {
+	pipe = '|';
+	var data = nav.get(mw, '');
+	rTo = /"([^"\s\.]{40})"/gm;
+	var token = '_token=' + rTo.exec(data)[1];
+	r64 = /"([a-z0-9]{32})",/gm;
+	var b64 = r64.exec(data)[1];
+	data = nav.postM(cw + "/" + b64, 'Referer|' + mw, token);
+	rId =/\/viewer\/([^/]+)/gm;
+	var id = rId.exec(data)[1];
+	src = nav.get("https://tmofans.com/viewer/" + id + "/cascade", 'Referer|' + cw);
+	rImg = /<img src="(https:\/\/img1.tmofans.com\/[^"]+)/gm;
+	oImg = "https://tmofans.com/viewer/" + id + "/cascade";
+	do {
+		i = rImg.exec(src);
+		if (i) {
+			oImg = oImg + pipe + i[1];
+		}
+	} while (i);
+	return oImg;
+};
+function cre1(){
+	return '<div class="col-10 text-truncate"[\s\S]+?(<.+?<\/a>)[\s\S]+?"_.+?\(\'([^\']+)';
+};
+function cre2(){
+	return '<div class="col-4 col-md-6 text-truncate">([^\']+)<\/span>[\s\S]+?"_.+?\(\'([^\']+)[\s\S])';
+};
