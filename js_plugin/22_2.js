1398701e1732bea6525339a402a261d86752e73b
raulhaag <2048339+raulhaag@users.noreply.github.com>
2019-11-26 14:55:29 -0300

    small improve

--- js_plugin/22_2.js
+++ js_plugin/22_2.js
@@ -10,7 +10,7 @@ chapterInit: 	function (cw, mw) {
 			token = token + "|" + rPo[1] + "|" + cw ;
 			rPo = tPo.exec(data);
 			token = token + "|" + rPo[1] + "|" + rPo[2] ;
-			rAc = /setAttribute\(["']action["'],\s*["']([^"']+)/gm;
+			rAc = /setAttribute\(["']action["'],\s*["'](https:\/\/tmofans.com\/[^"']+)/gm;
 			act = rAc.exec(data)[1];
 			data = nav.post(act, 'Connection|keep-alive|Referer|' + mw, token);
 			rId =/\/viewer\/([^/]+)/gm;
